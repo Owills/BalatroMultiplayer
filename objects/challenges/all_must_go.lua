6f5d9037601af3a5e3232942a1d78672b03648d4
Owills <oliver.w.marker@gmail.com>
2025-12-20 19:05:48 -0500

    rm from all_must_go

--- objects/challenges/all_must_go.lua
+++ objects/challenges/all_must_go.lua
@@ -3,7 +3,6 @@ SMODS.Challenge({
 	jokers = {
 		{ id = "j_mp_taxes", eternal = true },
 		{ id = "j_campfire", eternal = true },
-		{ id = "j_add_em_up", eternal = true },
 	},
 	unlocked = function(self)
 		return true
